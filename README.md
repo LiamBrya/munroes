fb177bc62fd8ecea9641830bbbc0276031aff893
BeUndead <beundead@hotmail.com>
2021-03-29 18:20:00 +0100

    Initial Commit.
    * Created SpringInitializr project for munroes.
    * Added some basic information to README.
    * Created basic gradle-base and added gradle-wrapper to repository.
    * Added preliminary .gitignore.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Munroes Project
+
+This library contains an implementation of the Munroes project,
+part of the interview process for xDesign.
+
+## Authors
+
+* Liam Bryan
---
+++ src/main/java/com/com/munroes/MunroesProjectApplication.java
@@ -0,0 +1,12 @@
+package com.com.munroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunroesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunroesProjectApplication.class, args);
+    }
+}
